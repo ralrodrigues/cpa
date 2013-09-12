06069f5a26851f84e608560717b8eb1bb5a022d0
renestvs <rene.stvs@gmail.com>
2013-09-12 17:11:59 -0300

    led

--- app/models/turma.rb
+++ app/models/turma.rb
@@ -2,11 +2,11 @@ class Turma < ActiveRecord::Base
   belongs_to :funcionario
   belongs_to :disciplina
 
-<<<<<<< HEAD
+
   has_many :turmas_alunos, dependent: :destroy
   
-=======
+
 	has_many :salas
 	has_many :usuarios, through: :salas
->>>>>>> upstream/master
+
 end
